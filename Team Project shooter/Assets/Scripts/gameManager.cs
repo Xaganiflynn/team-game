1434163b4fe0259dd5395b920a930eb3067630a1
Martin Alcantar <Martin.Alcantar.Pena@gmail.com>
2022-08-10 15:56:20 -0400

    SpawnPoint
    Spawn :D

---
+++ Team Project shooter/Assets/Scripts/gameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+
+    public GameObject player;
+    public playerController playerScript;
+    public GameObject playerSpawnPos;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<playerController>();
+
+        playerSpawnPos = GameObject.FindGameObjectWithTag("Player Spawn Pos");
+        playerScript.Respawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Team Project shooter/Assets/Scripts/playerController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [Header("----- Components -----")]
+    [SerializeField] CharacterController controller;
+
+    [Header("----- Player Stats -----")]
+    [Range(1, 10)][SerializeField] float playerSpeed;
+    [Range(1, 4)][SerializeField] float sprintMultiplyer;
+    [Range(8, 18)][SerializeField] float jumpHeight;
+    [Range(15, 30)][SerializeField] float gravity;
+    [Range(1, 3)][SerializeField] int jumpsMax;
+
+    private Vector3 playerVelocity;
+    Vector3 move = Vector3.zero;
+    int timesJumped;
+    float playerSpeedOriginal;
+    bool isSprinting = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSpeedOriginal = playerSpeed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMovement();
+        Sprint();
+    }
+
+    void playerMovement()
+    {
+        if (controller.isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+            timesJumped = 0;
+        }
+
+        //get input from Unity input system
+        move = (transform.right * Input.GetAxis("Horizontal")) + (transform.forward * Input.GetAxis("Vertical"));
+
+        // add our move vector to character controller
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && timesJumped < jumpsMax)
+        {
+            playerVelocity.y = jumpHeight;
+            timesJumped++;
+        }
+
+        playerVelocity.y -= gravity * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    void Sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            isSprinting = true;
+            playerSpeed = playerSpeed * sprintMultiplyer;
+        }
+        if (Input.GetButtonUp("Sprint"))
+        {
+            isSprinting = false;
+            playerSpeed = playerSpeedOriginal;
+        }
+    }
+
+    public void Respawn()
+    {
+        controller.enabled = false;
+
+        transform.position = gameManager.instance.playerSpawnPos.transform.position;
+
+        controller.enabled = true;
+    }
+}
